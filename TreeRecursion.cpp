1b67d493858f55f8c3cb2b12bcaaa59030e4b5f4
unknown <msin.aryan08@gmail.com>
2020-03-04 19:30:29 +0530

    Recursion

---
+++ TreeRecursion.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int fun(int n)	{
+		if(n>0)	{
+			cout<<n;
+			fun(n-1);
+			fun(n-1);
+		}
+}
+
+int main()	{
+		int n;
+		cin>>n;
+		fun(n);
+		return 0;
+}

aace2592bca283b1d8f71185da538c8d559925cd
Aryan <msin.aryan08@gmail.com>
2021-04-27 03:32:51 +0530

    Updated Graphs Section

--- Graphs/ToplogicalSort_BFS.cpp
+++ Graphs/ToplogicalSort_BFS.cpp
@@ -11,6 +11,34 @@ public:
 		l[x].push_back(y);
 	}
 
+	void printGraphList()	{
+		for(auto &it : l)	{
+			cout<<it.first<<" : ";
+			for(auto edge : it.second)	{
+				cout<<edge<<" ";
+			}
+			cout<<endl;
+		}
+	}
+
+
+	bool cycle_helper(int src, vector<bool> &visited, int parent)	{
+		visited[src] = true;
+		for(auto &nbr : l[src])	{
+			if(!visited[nbr])	{
+				if(cycle_helper(nbr, visited, src))	return true;
+			}
+			else if(nbr!=parent)	return true;
+		}
+		return false;
+	}
+
+	bool contains_cycle()	{
+		vector<bool> visited(l.size());
+		return cycle_helper(0, visited, -1);
+	}
+
+
 	void topological_sort()	{
 		//Calculating indegree of all nodes
 		vector<int> indegree(l.size());
@@ -24,7 +52,7 @@ public:
 		queue<int> q;
 		for(int i=0;i<l.size();++i)	{
 			if(indegree[i]==0)	{
-			//Pushin node to queue if it is not dependent on anyone i.e indegree=0
+			//Pushing node to queue if it is not dependent on anyone i.e indegree=0
 				q.push(i);
 			}
 		}
@@ -55,7 +83,12 @@ int main()	{
     	g.addEdge(2,3);
     	g.addEdge(3,4);
     	g.addEdge(4,5);
-    	g.topological_sort();
+    	g.addEdge(3,0);
+    	g.addEdge(2,7);
+    	g.printGraphList();
+    	cout<<g.contains_cycle()<<endl;
+    	if(g.contains_cycle())	cout<<"Cycle exists ";
+    	else g.topological_sort();
     	return 0;
 	
 }
